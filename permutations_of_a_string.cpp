8689aabc727bc669e297c395a7b60c300b20b7f1
Narendra <70422633+Narendra-13@users.noreply.github.com>
2022-12-13 14:33:30 +0530

    Create permutations_of_a_string.cpp

---
+++ permutations_of_a_string.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+	public:
+		vector<string>find_permutation(string S)
+		{
+		    sort(S.begin(), S.end());
+		    vector <string> next;
+		    do {  
+                next.push_back(S);  
+            } while(next_permutation(S.begin(), S.end()));
+            return next;
+		}
+};
